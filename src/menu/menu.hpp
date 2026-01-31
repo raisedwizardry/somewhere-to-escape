6dcd4e3d7868e00a1465639889daafb6835c6e10
raisedwizardry <fuzzy.gift9058@frozenincode.com>
2026-01-31 02:27:40 -0500

    update selection images wip

--- src/menu/menu.hpp
+++ src/menu/menu.hpp
@@ -11,6 +11,6 @@ class Menu {
     private:
         Sound& _sound;
         sprite_t *ramHead = sprite_load("rom://select-ram.sprite");
-        sprite_t *bunnyHead = sprite_load("rom://select-ram.sprite");
+        sprite_t *bunnyHead = sprite_load("rom://select-bunny.sprite");
         sprite_t *squirHead = sprite_load("rom://select-ram.sprite");
 };
