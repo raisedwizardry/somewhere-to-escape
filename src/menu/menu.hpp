fd18fa4d19097ca72dd534d9836865a9637709fb
raisedwizardry <fuzzy.gift9058@frozenincode.com>
2026-01-31 01:43:07 -0500

    update assets for mistakenly added videos

--- src/menu/menu.hpp
+++ src/menu/menu.hpp
@@ -10,7 +10,7 @@ class Menu {
         void selectCharacter();
     private:
         Sound& _sound;
-        sprite_t *ramHead = sprite_load("rom://ram-headshot-s.sprite");
-        sprite_t *bunnyHead = sprite_load("rom://ram-headshot-resize3.sprite");
-        sprite_t *squirHead = sprite_load("rom://ram-headshot-resize3.sprite");
+        sprite_t *ramHead = sprite_load("rom://select-ram.sprite");
+        sprite_t *bunnyHead = sprite_load("rom://select-ram.sprite");
+        sprite_t *squirHead = sprite_load("rom://select-ram.sprite");
 };
